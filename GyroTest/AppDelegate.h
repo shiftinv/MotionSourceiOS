a45c7bb3fda6ac17dce7640a5446d932373b7664
shiftinv <me@shiftinv.cc>
2017-10-28 19:08:14 +0200

    Initial Commit

---
+++ GyroTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GyroTest
+//
+//  Created on 28.10.17.
+//  Copyright © 2017 All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GyroTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GyroTest
+//
+//  Created on 28.10.17.
+//  Copyright © 2017 All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
